2f3adc30516f1988186f9224dc886fa4334d1d2b
LinerFox <109337208+LinerFox@users.noreply.github.com>
2023-05-20 17:38:31 -0400

    Finished Setup
    Starting to import data using GraphQL. Finished setup of main layout for now.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import "../styles/global.scss";
+
+import React, { useEffect, useState } from "react";
+
+import { Layout } from "../components";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
